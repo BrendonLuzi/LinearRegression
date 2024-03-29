c8315722f58c3cdb90ff38d243e5b0f4e3ff10c6
bluzi <bluzi@student.42heilbronn.de>
2024-03-29 16:28:13 +0100

    v 1.0

--- training.py
+++ training.py
@@ -2,12 +2,14 @@ import numpy
 import pandas as pandas
 import matplotlib.pyplot as pyplot
 
-def save_parameters(price_min, price_max, thtea0, theta1):
+def save_parameters(price_min, price_max, mileage_min, mileage_max, theta0, theta1):
 	param = open('parameters.txt', 'w')
 	param.truncate()
 	param.write('{\n')
 	param.write(f'\tMin price: {price_min}\n')
 	param.write(f'\tMax price: {price_max}\n')
+	param.write(f'\tMin mileage: {mileage_min}\n')
+	param.write(f'\tMax mileage: {mileage_max}\n')
 	param.write(f'\tTheta0: {theta0}\n')
 	param.write(f'\tTheta1: {theta1}\n')
 	param.write('}')
@@ -26,8 +28,8 @@ def linear_regression(x, y, n, learning_speed):
 	# Initialize mean sqaured error variable
 	mse = 0
 
-	# Loop until convergence or 100000 epochs
-	for _ in range(100000):
+	# Loop until convergence or 1000000 epochs
+	for _ in range(1000000):
 		# Predictions of the y values acoording to the temporary parameters
 		est_y = x * theta1 + theta0
 		# Error estimation
@@ -36,7 +38,7 @@ def linear_regression(x, y, n, learning_speed):
 		# Calculation of the mean squared error to determine if we reached convergence
 		new_mse = (1 / n) * numpy.sum(error ** 2)
 		# If the difference in the mse is small enough, convergence has been reached, quit the loop
-		if abs(new_mse - mse) < 0.0000000001:
+		if abs(new_mse - mse) < 0.00000000001:
 			break
 		# If not, update the mse value
 		mse = new_mse
@@ -52,10 +54,13 @@ def linear_regression(x, y, n, learning_speed):
 	return theta0, theta1;
 
 # Read the dataset and define the initial parameters
-dataset = pandas.read_csv("data.csv")
+try:
+	dataset = pandas.read_csv("data.csv")
 
-price = dataset['price']
-mileage = dataset['km']
+	price = dataset['price']
+	mileage = dataset['km']
+except:
+	print('Error reading the dataset!'), exit()
 
 # Normalize the values
 norm_price, norm_mileage = normalize(price), normalize(mileage)
@@ -68,10 +73,11 @@ learning_speed = 0.001
 theta0, theta1 = linear_regression(numpy.array(norm_mileage), numpy.array(norm_price), n, learning_speed)
 
 # Save the parameters
-save_parameters(price.min(), price.max(), theta0, theta1)
+save_parameters(price.min(), price.max(), mileage.min(), mileage.max(), theta0, theta1)
 
-# Plot the results
-pyplot.scatter(norm_mileage, norm_price)
-pyplot.plot(norm_mileage, (norm_mileage * theta1) + theta0, color='red')
+if input('Model trained! Input \'y\' to see the results plotted...', ) == 'y':
+	# Plot the results
+	pyplot.scatter(norm_mileage, norm_price)
+	pyplot.plot(norm_mileage, (norm_mileage * theta1) + theta0, color='red')
 
-pyplot.show()
+	pyplot.show()
