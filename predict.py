e1c2ad61155670d8badaba83f562f9617ffc565e
bluzi <bluzi@student.42heilbronn.de>
2024-03-30 15:01:47 +0100

    Minor improvements + json

--- predict.py
+++ predict.py
@@ -1,14 +1,12 @@
+import json
+
 # Function to retrieve the parameters found from the training
 def get_params():
 	try:
-		p_file = open('parameters.txt')
-		parameters = p_file.readlines()[1:-1]
+		with open('parameters.json', 'r') as file:
+			parameters = json.load(file)
 	except:
 		print('No parameters found, try training the model first!'), exit()
-
-	# For every string, trim and find the value, convert to float and store it
-	for i, param in enumerate(parameters):
-		parameters[i] = float(param[param.find(':') + 1:])
 
 	return parameters
 
@@ -23,20 +21,11 @@ if (mileage < 0):
 
 # Get params
 parameters = get_params()
-p_price = parameters[:2]
-p_mileage = parameters[2:4]
-theta = parameters[4:]
-
-# Normalize according to the training model params
-mileage = (mileage - p_mileage[0]) / (p_mileage[1] - p_mileage[0])
 
 # Estimate price
-price = theta[1] * mileage + theta[0]
-
-# Denormalize the price and round the price
-price = price * (p_price[1] - p_price[0]) + p_price[0]
-price = round(price)
+price = round(parameters['Theta1'] * mileage + parameters['Theta0'])
 
 if price <= 0:
-	print('Maybe it\'s time to get rid of it...')
-print(f'Your car should be valued around {price} euro!')
+	print('Ehm... maybe it\'s time to get rid of it...')
+else:
+	print(f'Your car should be valued around {price} euro!')
--- training.py
+++ training.py
@@ -1,83 +1,93 @@
 import numpy
+import json
 import pandas as pandas
 import matplotlib.pyplot as pyplot
 
-def save_parameters(price_min, price_max, mileage_min, mileage_max, theta0, theta1):
-	param = open('parameters.txt', 'w')
-	param.truncate()
-	param.write('{\n')
-	param.write(f'\tMin price: {price_min}\n')
-	param.write(f'\tMax price: {price_max}\n')
-	param.write(f'\tMin mileage: {mileage_min}\n')
-	param.write(f'\tMax mileage: {mileage_max}\n')
-	param.write(f'\tTheta0: {theta0}\n')
-	param.write(f'\tTheta1: {theta1}\n')
-	param.write('}')
-	param.close()
+EPOCHS_LIMIT = 1000000
+PRECISION = 0.00000000001
+LEARNING_SPEED = 0.001
+
+def save_parameters(theta0, theta1):
+	params = {
+		"Theta0": theta0,
+		"Theta1": theta1
+	}
+	with open("parameters.json", "w") as file:
+		json.dump(params, file)
 
 # Normalize function
 def normalize(values):
 	return (values - values.min()) / (values.max() - values.min())
 
-# Linear regression
-def linear_regression(x, y, n, learning_speed):
+# Mean squared error
+def mean_squared_error(y, est_y):
+	return numpy.mean((y - est_y) ** 2)
+
+# Gradient descent
+def gradient_descent(x, y, n):
 	# Initial parameters of the line that describes the data where price = mileage * theta1 + theta0
 	theta0 = 0.0
 	theta1 = 0.0
-
 	# Initialize mean sqaured error variable
 	mse = 0
 
-	# Loop until convergence or 1000000 epochs
-	for _ in range(1000000):
+	# Loop until convergence or until a predetermined amount of iterations
+	for _ in range(EPOCHS_LIMIT):
 		# Predictions of the y values acoording to the temporary parameters
 		est_y = x * theta1 + theta0
 		# Error estimation
 		error = est_y - y
 
 		# Calculation of the mean squared error to determine if we reached convergence
-		new_mse = (1 / n) * numpy.sum(error ** 2)
+		new_mse = mean_squared_error(y, est_y)
 		# If the difference in the mse is small enough, convergence has been reached, quit the loop
-		if abs(new_mse - mse) < 0.00000000001:
+		if abs(new_mse - mse) < PRECISION:
 			break
 		# If not, update the mse value
 		mse = new_mse
 
 		# Calculation of the gradients
-		grad_theta0 = (1 / n) * numpy.sum(error)
-		grad_theta1 = (1 / n) * numpy.sum(error * x)
+		grad_theta0 = numpy.sum(error) / n
+		grad_theta1 = numpy.sum(error * x) / n
 		
 		# Update the parameters
-		theta0 = theta0 - learning_speed * grad_theta0;
-		theta1 = theta1 - learning_speed * grad_theta1;
+		theta0 = theta0 - LEARNING_SPEED * grad_theta0
+		theta1 = theta1 - LEARNING_SPEED * grad_theta1
 
-	return theta0, theta1;
+	return theta0, theta1
 
-# Read the dataset and define the initial parameters
-try:
-	dataset = pandas.read_csv("data.csv")
-
-	price = dataset['price']
-	mileage = dataset['km']
-except:
-	print('Error reading the dataset!'), exit()
-
-# Normalize the values
-norm_price, norm_mileage = normalize(price), normalize(mileage)
-
-n = len(price)
-
-learning_speed = 0.001
-
-# Compute the parameters training the model
-theta0, theta1 = linear_regression(numpy.array(norm_mileage), numpy.array(norm_price), n, learning_speed)
-
-# Save the parameters
-save_parameters(price.min(), price.max(), mileage.min(), mileage.max(), theta0, theta1)
-
-if input('Model trained! Input \'y\' to see the results plotted...', ) == 'y':
+def plot_results(x, y, theta0, theta1):
 	# Plot the results
-	pyplot.scatter(norm_mileage, norm_price)
-	pyplot.plot(norm_mileage, (norm_mileage * theta1) + theta0, color='red')
+	pyplot.scatter(x, y)
+	pyplot.plot(x, (x * theta1) + theta0, color='red')
 
 	pyplot.show()
+
+def main():
+	# Read the dataset and define the initial parameters
+	try:
+		dataset = pandas.read_csv("data.csv")
+
+		price = dataset['price']
+		mileage = dataset['km']
+	except:
+		print('Error reading the dataset!'), exit()
+
+	# Normalize the values
+	norm_price, norm_mileage = normalize(price), normalize(mileage)
+
+	# Compute the parameters training the model
+	theta0, theta1 = gradient_descent(numpy.array(norm_mileage), numpy.array(norm_price), len(norm_price))
+
+	# Denormalize the parameters
+	theta1 = theta1 * (price.max() - price.min()) / (mileage.max() - mileage.min())
+	theta0 = theta0 * (price.max() - price.min()) + price.min() - theta1 * mileage.min()
+
+	# Save the parameters
+	save_parameters(theta0, theta1)
+
+	if input('Model trained! Input \'y\' to see the results plotted...', ) == 'y':
+		plot_results(mileage, price, theta0, theta1)
+
+if __name__ == '__main__':
+	main()
